4c6907c646d09f98aeb3c7c487e29b48c63d984b
byunn90 <kayhanturk90@Live.com>
2024-03-11 22:39:13 +1100

    div working with css

--- my-textcp/src/App.js
+++ my-textcp/src/App.js
@@ -4,47 +4,51 @@ import "./index.css";
 export default function App() {
   return (
     <div>
-      <TextExpander
-        expanded={true}
-        collapsedNumWords={20}
-        expandButtonText="Show text"
-        collapseButtonText="Collapse text"
-        buttonColor="#ff6622"
-      >
-        Space travel is the ultimate adventure! Imagine soaring past the stars
-        and exploring new worlds. It's the stuff of dreams and science fiction,
-        but believe it or not, space travel is a real thing. Humans and robots
-        are constantly venturing out into the cosmos to uncover its secrets and
-        push the boundaries of what's possible.
-      </TextExpander>
+      <div className="box">
+        <TextExpander
+          expanded={true}
+          collapsedNumWords={20}
+          expandButtonText="Show text"
+          collapseButtonText="Collapse text"
+          buttonColor="#ff6622"
+        >
+          Space travel is the ultimate adventure! Imagine soaring past the stars
+          and exploring new worlds. It's the stuff of dreams and science
+          fiction, but believe it or not, space travel is a real thing. Humans
+          and robots are constantly venturing out into the cosmos to uncover its
+          secrets and push the boundaries of what's possible.
+        </TextExpander>
 
-      <TextExpander
-        expanded={true}
-        collapsedNumWords={20}
-        expandButtonText="Show text"
-        collapseButtonText="Collapse text"
-        buttonColor="#ff6622"
-      >
-        Space travel requires some seriously amazing technology and
-        collaboration between countries, private companies, and international
-        space organizations. And while it's not always easy (or cheap), the
-        results are out of this world. Think about the first time humans stepped
-        foot on the moon or when rovers were sent to roam around on Mars.
-      </TextExpander>
-
-      <TextExpander
-        expanded={true}
-        collapsedNumWords={20}
-        expandButtonText="Show text"
-        collapseButtonText="Collapse text"
-        buttonColor="#ff6622"
-        className="box"
-      >
-        Space missions have given us incredible insights into our universe and
-        have inspired future generations to keep reaching for the stars. Space
-        travel is a pretty cool thing to think about. Who knows what we'll
-        discover next!
-      </TextExpander>
+        <TextExpander
+          expanded={true}
+          collapsedNumWords={20}
+          expandButtonText="Show text"
+          collapseButtonText="Collapse text"
+          buttonColor="#ff6622"
+        >
+          Space travel requires some seriously amazing technology and
+          collaboration between countries, private companies, and international
+          space organizations. And while it's not always easy (or cheap), the
+          results are out of this world. Think about the first time humans
+          stepped foot on the moon or when rovers were sent to roam around on
+          Mars.
+        </TextExpander>
+      </div>
+      <div className="box">
+        <TextExpander
+          expanded={true}
+          collapsedNumWords={20}
+          expandButtonText="Show text"
+          collapseButtonText="Collapse text"
+          buttonColor="#ff6622"
+          className="box"
+        >
+          Space missions have given us incredible insights into our universe and
+          have inspired future generations to keep reaching for the stars. Space
+          travel is a pretty cool thing to think about. Who knows what we'll
+          discover next!
+        </TextExpander>
+      </div>
     </div>
   );
 }
@@ -54,12 +58,10 @@ function TextExpander({
   collapsedNumWords,
   expandButtonText,
   collapseButtonText,
-  buttonColor,
   children,
-  box,
+  color,
 }) {
   const [expanded, setExpanded] = useState(false);
-
   const toggleExpand = () => {
     setExpanded(!expanded);
   };
@@ -69,7 +71,7 @@ function TextExpander({
       <span>
         {expanded ? children : children.substring(0, collapsedNumWords)}
       </span>
-      <button onClick={toggleExpand}>
+      <button buttonColor={color} onClick={toggleExpand}>
         {expanded ? collapseButtonText : expandButtonText}
       </button>
     </div>
